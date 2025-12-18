ea058a3364173d0988da18ecac6ccf1614ca2b70
rajviajmera68-netizen <rajviajmera68@gmail.com>
2025-12-18 19:26:39 +0530

    my programm's

--- List_Comprehension.py
+++ List_Comprehension.py
@@ -1,4 +1,12 @@
 # List Comprehension 
-list1=[2,4,6,8,10]
+#------------> noramal LIST <------------
+'''list1=[2,4,6,8,10]
 value=[val **3 for val in list1]
-print(value)
+print(value)'''
+
+#-----------> By Using LOOP <-------------
+list2=[1,2,3,4,5,6]
+value2=[]
+for i in list2:
+	value2.append(i*2)
+print(value2)
