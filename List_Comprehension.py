7301cc03eafbbc58a156fccd66eca3169c182cd4
rajviajmera68-netizen <rajviajmera68@gmail.com>
2025-12-18 19:34:56 +0530

    my programm's

--- List_Comprehension.py
+++ List_Comprehension.py
@@ -1,6 +1,6 @@
 # List Comprehension 
 #------------> noramal LIST <------------
-list1=[2,4,6,8,10]
+'''list1=[2,4,6,8,10]
 value=[val **3 for val in list1]
 print(value)
 
@@ -9,4 +9,11 @@ list2=[1,2,3,4,5,6]
 value2=[]
 for i in list2:
 	value2.append(i*2)
-print(value2)
+print(value2)'''
+
+# ---->>>Example Explanation:-> value3 = [j * 2 for j in list3] 
+#use list comprehension to create a new list by (doubling) each number in a 
+# In place 2 anything can be witten (2 means doublind,3 mean thriple ,...etc.)<<<-----
+ist3=[1,3,5,7,4]
+value3=[j *3 for j in list3]
+print(value3)
