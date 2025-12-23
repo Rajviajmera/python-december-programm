32e26a9c4a672df3676d141b6844727c019b4b37
rajviajmera68-netizen <rajviajmera68@gmail.com>
2025-12-23 19:46:03 +0530

    my programm

--- exception_handling.py
+++ exception_handling.py
@@ -3,10 +3,30 @@ try:
 	a=int(input("enter value:"))
 	b=int(input("enter value:"))
 	c=a/b
-	print("{}/{}={}".format(a,b,c))
+	
 except ZeroDivisionError :
 	print("Denominator can't be Zero")
 except ValueError :
 	print("Only Integer value are allowed")
+else:
+	print("{}/{}={}".format(a,b,c))
 finally:
 	print("exection completed succesfully")
+
+#for factorial
+try:
+	x=int(input("enter value:"))
+	if x<0:
+		raise ValueError ("-ve value not allowed")
+	else:
+		fact=1
+		for i in range(1,x+1):
+			fact=fact*i
+		print("{}!={}".format(x,fact))
+
+#except ValueError as e :
+#	print(e)
+
+except ValueError:
+	print("something went wrong")
+
