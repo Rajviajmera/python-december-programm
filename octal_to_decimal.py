bc4e1115db488ce849697b9389ab8c8c4d83e123
rajviajmera68-netizen <rajviajmera68@gmail.com>
2026-01-07 19:17:13 +0530

    my programm

---
+++ octal_to_decimal.py
@@ -0,0 +1,8 @@
+# Take octal number as input
+octal_num = input("Enter an octal number: ")
+
+# Convert octal to decimal
+decimal_num = int(octal_num, 8)
+
+# Display result
+print("Decimal equivalent:", decimal_num)
